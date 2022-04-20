29a3024c2a7704553f9548f910a4941209b6f0b0
Gabriel Baraboi <gabrielbaraboi5@gmail.com>
2022-04-21 02:01:05 +0300

    created start, login, register and lost password screens

--- client/src/navigation/AccountNavigator.js
+++ client/src/navigation/AccountNavigator.js
@@ -4,6 +4,8 @@ import { createStackNavigator } from "@react-navigation/stack";
 import AccountScreen from "../screens/account/AccountScreen";
 import LoginScreen from "../screens/account/LoginScreen";
 import RegisterScreen from "../screens/account/RegisterScreen";
+import HomeScreen from "../screens/HomeScreen";
+import ResetPasswordScreen from "../screens/account/ResetPasswordScreen";
 
 const Stack = createStackNavigator();
 
@@ -12,12 +14,17 @@ const AccountNavigator = () => {
 		<Stack.Navigator
 			initialRouteName="AccountScreen"
 			screenOptions={{
-				headerShown: true,
+				headerShown: false,
 			}}
 		>
 			<Stack.Screen name="AccountScreen" component={AccountScreen} />
-			<Stack.Screen name="Login" component={LoginScreen} />
-			<Stack.Screen name="Register" component={RegisterScreen} />
+			<Stack.Screen name="HomeScreen" component={HomeScreen} />
+			<Stack.Screen name="LoginScreen" component={LoginScreen} />
+			<Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+			<Stack.Screen
+				name="ResetPasswordScreen"
+				component={ResetPasswordScreen}
+			/>
 		</Stack.Navigator>
 	);
 };
--- client/src/screens/account/AccountScreen.js
+++ client/src/screens/account/AccountScreen.js
@@ -1,11 +1,29 @@
 import React from "react";
-import { View, Text } from "react-native";
+import Background from "../../components/Background";
+import Header from "../../components/Header";
+import Paragraph from "../../components/Paragraph";
+import Button from "../../components/Button";
+import Logo from "../../components/Logo";
 
-const AccountScreen = () => {
+const AccountScreen = ({ navigation }) => {
 	return (
-		<View>
-			<Text>AccountScreen</Text>
-		</View>
+		<Background>
+			<Logo />
+			<Header>SmartCooker</Header>
+			<Paragraph>The easiest way to find out what to cook.</Paragraph>
+			<Button
+				mode="contained"
+				onPress={() => navigation.navigate("LoginScreen")}
+			>
+				Login
+			</Button>
+			<Button
+				mode="outlined"
+				onPress={() => navigation.navigate("RegisterScreen")}
+			>
+				Sign Up
+			</Button>
+		</Background>
 	);
 };
 
